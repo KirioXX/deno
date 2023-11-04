13d9467b7659ab3ffeabd75d929452193c820ff3
Percy <percy@railway.app>
2023-11-04 22:59:03 +0000

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { serve } from "https://deno.land/std@0.176.0/http/server.ts";
+
+let port = parseInt(Deno.env.get("PORT") ?? "8000");
+const s = serve({ port });
+
+console.log(`http://localhost:${port}/`);
+
+for await (const req of s) {
+	req.respond({ body: "Choo Choo! Welcome to your Deno app\n" });
+}
